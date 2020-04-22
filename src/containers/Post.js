402783ecea00ec24300c79a528044725d38ac4c0
Timothy Ellersiek <timothy@openrouteservice.org>
2020-04-22 22:25:43 +0200

    update which makes it work

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,14 +1,64 @@
 import React from 'react'
 import { useRouteData } from 'react-static'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-
 //
+
 import { Link } from 'components/Router'
-// Importing Leaflet Destroys the dynamic generation of the Html Head
 import Head from 'react-helmet'
+import './leaflet.css'  
+
 
 const Post = () => {
   const { post } = useRouteData()
+
+  if (typeof document !== 'undefined') {
+
+    const a = './leaflet-src.esm.js'    
+
+    import(`${a}`).then(L => {
+
+      const southWest = L.latLng(-89.98155760646617, -180)
+      const northEast = L.latLng(89.99346179538875, 180)
+      const bounds = L.latLngBounds(southWest, northEast)
+
+      const mapParams = {
+        //center: [25.95681, -35.729687],
+        center: [40.419, -73.889],
+        maxBounds: bounds,
+        zoom: 10,
+        minZoom: 2,
+        maxZoom: 18
+      }
+
+      const map = L.map('map', mapParams)
+
+
+
+   
+      map.setView(mapParams.center, 6)
+        
+      L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
+        {
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          subdomains: 'abcd',
+          maxZoom: 19
+        }
+      ).addTo(map)
+
+      
+      })
+    
+  } else {
+
+    console.log('react-static cant do leaflet')
+
+  }
+
+
+
+
+
   return (
     <div>
       <Head>
@@ -18,8 +68,10 @@ const Post = () => {
       <br />
       <h3>{post.title}</h3>
       <p>{post.body}</p>
+      <div style={{ height: '300px', width: '300px'}} id="map" />
     </div>
   )
 }
 
 export default Post
+
